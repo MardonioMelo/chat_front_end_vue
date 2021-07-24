3159eaef80df54b0abf34db5520732776dcf9c0f
MardonioMelo <mardonio.quimico@gmail.com>
2021-07-24 20:22:00 -0300

    update

--- src/components/NavbarItem.vue
+++ src/components/NavbarItem.vue
@@ -7,31 +7,36 @@
           <button class="btn">
             <img class="avatar-xl" :src="avatar" alt="avatar" />
           </button>
-          <a href="#members" data-toggle="tab"
-            ><span class="material-icons"
-              ><i class="fa fa-user-circle" aria-hidden="true"></i></span
-          ></a>
-          <a href="#discussions" data-toggle="tab" class="active"
-            ><span class="material-icons"
-              ><i class="far fa-comments" aria-hidden="true"></i></span
-          ></a>
-          <a href="#notifications" data-toggle="tab" class="f-grow1"
-            ><span class="material-icons"
-              ><i class="far fa-bell" aria-hidden="true"></i></span
-          ></a>
-          <button class="btn mode">
-            <span class="material-icons"
-              ><i class="fas fa-moon" aria-hidden="true"></i
-            ></span>
-          </button>
-          <a href="#settings" data-toggle="tab"
-            ><span class="material-icons"
-              ><i class="fas fa-cog" aria-hidden="true"></i></span
-          ></a>
-          <router-link class="btn power" to="/login"
-            ><span class="material-icons"
-              ><i class="fas fa-power-off" aria-hidden="true"></i></span
-          ></router-link>
+          <a href="#members" data-toggle="tab">
+            <span class="material-icons">
+              <i class="fa fa-user-circle" aria-hidden="true"></i>
+            </span>
+          </a>
+          <a href="#discussions" data-toggle="tab" class="active">
+            <span class="material-icons">
+              <i class="far fa-comments" aria-hidden="true"> </i>
+            </span>
+          </a>
+          <a href="#notifications" data-toggle="tab" class="f-grow1">
+            <span class="material-icons">
+              <i class="far fa-bell" aria-hidden="true"></i>
+            </span>
+          </a>
+          <!-- <button class="btn mode">
+            <span class="material-icons">
+              <i class="fas fa-moon" aria-hidden="true"></i>
+            </span>
+          </button> -->
+          <a href="#settings" data-toggle="tab">
+            <span class="material-icons">
+              <i class="fas fa-cog" aria-hidden="true"></i>
+            </span>
+          </a>
+          <router-link class="btn power" to="/login">
+            <span class="material-icons">
+              <i class="fas fa-power-off" aria-hidden="true"></i>
+            </span>
+          </router-link>
         </div>
       </div>
     </div>
