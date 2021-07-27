a31ea87160d3dd325dd387741ed2c27b35cda414
MardonioMelo <mardonio.quimico@gmail.com>
2021-07-26 22:33:16 -0300

    Consulta e listagem da lista de espera

--- src/components/DiscussionsItem.vue
+++ src/components/DiscussionsItem.vue
@@ -9,56 +9,25 @@
     </div>   			
     <div class="discussions">
         <h1>Fila de Espera</h1>
-        <div class="list-group" id="chats" role="tablist">
-            <a href="#list-chat" class="filterDiscussions all unread single active" id="list-chat-list" data-toggle="list" role="tab">
-                <img class="avatar-md" src="https://stafforgserv.com.au/wp-content/uploads/2018/09/user-img.png" data-toggle="tooltip" data-placement="top" title="Janette" alt="avatar">
+        <div class="list-group" id="chats" role="tablist">    									
+            
+            <a v-for="client in clients" :key="client.id" href="#list-chat" class="filterDiscussions all unread single" id="list-chat-list5" data-toggle="list" role="tab">
+                <img class="avatar-md" src="https://stafforgserv.com.au/wp-content/uploads/2018/09/user-img.png" data-toggle="tooltip" data-placement="top" title="Mildred" alt="avatar">
                              
                 <div class="data">
-                    <h5>name</h5>
-                    <span>dia_semana</span>
-                    <p>mensagem</p>
+                    <h5>{{client.name}}</h5>
+                    <span>{{client.date}}</span>
+                    <p>{{client.description}}</p>
                 </div>
-            </a>									
-            
-            <a href="#list-chat" class="filterDiscussions all unread single" id="list-chat-list5" data-toggle="list" role="tab">
-                <img class="avatar-md" src="https://stafforgserv.com.au/wp-content/uploads/2018/09/user-img.png" data-toggle="tooltip" data-placement="top" title="Mildred" alt="avatar">
-                <div class="status">
-                    <span class="material-icons offline"><i class="fas fa-record-vinyl" aria-hidden="true"></i></span> 											
-                </div>
-                <div class="new bg-green">
-                    <span>+9</span>
-                </div>
-                <div class="data">
-                    <h5>Leonidas 300</h5>
-                    <span>Ter</span>
-                    <p>Aqui é sparta!</p>
-                </div>
-            </a>
+            </a>           
 
-            <a href="#list-chat" class="filterDiscussions all read single" id="list-chat-list5" data-toggle="list" role="tab">
+            <a v-for="client in clients" :key="client.id" href="#list-chat" class="filterDiscussions all read single" id="list-chat-list5" data-toggle="list" role="tab">
                 <img class="avatar-md" src="https://stafforgserv.com.au/wp-content/uploads/2018/09/user-img.png" data-toggle="tooltip" data-placement="top" title="Mildred" alt="avatar">
-                <div class="status">
-                        <span class="material-icons offline"><i class="fas fa-record-vinyl" aria-hidden="true"></i></span> 											
-                </div>   
-                <div class="new bg-gray">
-                    <span>?</span>
-                </div>                                    
-                <div class="data">
-                    <h5>Abel Marcks</h5>
-                    <span>Ter</span>
-                    <p>Hoje não irei cantar!</p>
-                </div>
-            </a>
-
-            <a href="#list-chat" class="filterDiscussions all read single" id="list-chat-list5" data-toggle="list" role="tab">
-                <img class="avatar-md" src="https://stafforgserv.com.au/wp-content/uploads/2018/09/user-img.png" data-toggle="tooltip" data-placement="top" title="Mildred" alt="avatar">
-                <div class="status">
-                        <span class="material-icons online"><i class="fas fa-record-vinyl" aria-hidden="true"></i></span> 											
-                </div>                                                                      
-                <div class="data">
-                    <h5>Homo Sapiens</h5>
-                    <span>Ter</span>
-                    <p>Vamos evoluir mano, é preciso!</p>
+                                                                                     
+                 <div class="data">
+                    <h5>{{client.name}}</h5>
+                    <span>{{client.date}}</span>
+                    <p>{{client.description}}</p>
                 </div>
             </a>
         </div>
@@ -69,7 +38,28 @@
 
 <script>
 export default {
-    name: "DiscussionsItem",   
+    name: "DiscussionsItem", 
+    data() {
+        return {
+            clients: []
+        }
+    },  
+    methods: {
+        async getRowClients(){
+            const req = await fetch("http://localhost:3000/row")
+            const res = await req.json()          
+
+            if(res.result){
+                console.log(res.error.msg)
+                 this.clients = res.error.data;
+            }else{
+              console.log(res.error.msg)
+            }          
+        }
+    },
+    mounted(){
+        this.getRowClients()
+    }
 }
 </script>
 
