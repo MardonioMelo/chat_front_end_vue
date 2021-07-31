1914de5ef8b6b5e123ef202f9e9657169a15d78d
MardonioMelo <mardonio.quimico@gmail.com>
2021-07-31 13:44:34 -0300

    update lista de clientes em espera

--- src/components/DiscussionsItem.vue
+++ src/components/DiscussionsItem.vue
@@ -9,8 +9,7 @@
     </div>   			
     <div class="discussions">
         <h1>Fila de Espera</h1>
-        <div class="list-group" id="chats" role="tablist">    									
-            
+        <div class="list-group" id="chats" role="tablist">          
             <a v-for="client in clients" :key="client.id" href="#list-chat" class="filterDiscussions all unread single" id="list-chat-list5" data-toggle="list" role="tab">
                 <img class="avatar-md" :src="client.avatar" data-toggle="tooltip" data-placement="top" title="Mildred" alt="avatar">
                              
@@ -20,16 +19,6 @@
                     <p>{{client.description}}</p>
                 </div>
             </a>           
-
-            <a v-for="client in clients" :key="client.id" href="#list-chat" class="filterDiscussions all read single" id="list-chat-list5" data-toggle="list" role="tab">
-                <img class="avatar-md" :src="client.avatar" data-toggle="tooltip" data-placement="top" title="Mildred" alt="avatar">
-                                                                                     
-                 <div class="data">
-                    <h5>{{client.name}}</h5>
-                    <span>{{client.date}}</span>
-                    <p>{{client.description}}</p>
-                </div>
-            </a>
         </div>
     </div>
 </div>
